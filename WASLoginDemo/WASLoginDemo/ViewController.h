277917ab15c6f589bb83efe2bb7d7c606e2bd34e
王璋杰 <398148947@qq.com>
2017-08-22 21:44:11 +0800

    first create

---
+++ WASLoginDemo/WASLoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WASLoginDemo
+//
+//  Created by 王璋杰 on 2017/8/22.
+//  Copyright © 2017年 sec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WASLoginDemo/WASLoginDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WASLoginDemo
+//
+//  Created by 王璋杰 on 2017/8/22.
+//  Copyright © 2017年 sec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
